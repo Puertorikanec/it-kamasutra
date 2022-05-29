dcd299179c7b8b0c5fd83f005a316f3e0cbbbab2
puertorikanec <poblasov@yandex.ru>
2022-05-29 11:31:54 +0300

    23

--- freecodecamp.js
+++ freecodecamp.js
@@ -136,3 +136,16 @@ c *= 1;
 c = c * 10;
 
 // 23
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 1; 
+a = a / 12;
+b /= 1; 
+b = b / 4;
+c /= 1; 
+c = c / 11;
+
+// 24
