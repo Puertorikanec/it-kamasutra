0b9256463e645500abe1bbfc152c2787fbcfeb0f
puertorikanec <poblasov@yandex.ru>
2022-05-29 13:13:46 +0300

    32

--- freecodecamp.js
+++ freecodecamp.js
@@ -182,3 +182,11 @@ const lastName = "Lovelace";
 // Only change code below this line
 lastNameLength = lastName.length;
 // 32
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
+
+// 33
